f76ad915c6d259ad5e275917dfcd20cbc1ce43ea
dqhieu <dqhieu13@gmail.com>
2022-03-09 20:13:22 +0800

    Initial Commit

---
+++ BiometricAuthentication/BiometricAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricAuthenticationApp.swift
+//  BiometricAuthentication
+//
+//  Created by Dinh Quang Hieu on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BiometricAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiometricAuthentication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BiometricAuthentication
+//
+//  Created by Dinh Quang Hieu on 09/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
